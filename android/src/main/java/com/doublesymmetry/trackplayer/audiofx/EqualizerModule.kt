b98a6d0847967d40a56cb56c66ff0551422f00bb
krishnanx <abhishekkrishnan2006@gmail.com>
2025-12-29 11:32:57 +0530

    chore: added imports for EQ module

--- android/src/main/java/com/doublesymmetry/trackplayer/audiofx/EqualizerModule.kt
+++ android/src/main/java/com/doublesymmetry/trackplayer/audiofx/EqualizerModule.kt
@@ -1,3 +1,13 @@
+package com.doublesymmetry.trackplayer.audiofx
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.Arguments
+import com.doublesymmetry.kotlinaudio.audiofx.EqualizerManager
+
+
 class EqualizerModule(
     reactContext: ReactApplicationContext
 ) : ReactContextBaseJavaModule(reactContext) {
